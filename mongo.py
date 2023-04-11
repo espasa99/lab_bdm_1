1b4a1e80179a50db283876dbbc85a2146cac2679
G0002 <guillem.bosch.tobella@estudiantat.upc.edu>
2023-04-11 19:42:39 +0200

    mongo.py hehe

--- mongo.py
+++ mongo.py
@@ -2,24 +2,29 @@ import json
 import os
 from pymongo import MongoClient
 # from os import listdir
+# import pandas
 
 directory = "idealista"
-
+# directory = "opendatabcn-income"
+#idealista: mirar duplicats, afegir la data, fer que propertycode sigui el id.
 
 client = MongoClient()
 db = client['PLZ']
 collection = db[directory]
-collection.remove({})
-# print("ara hi ha ", collection.count_documents({}), "documents")
+collection.delete_many({}) # per deletejar tots els docs de la collection
+# collection.drop() # per dropejar la collection
+print("ara hi ha ", collection.count_documents({}), "documents")
 
 for f in os.listdir(directory):
 
     with open(os.path.join(directory, f), 'r') as file:
+        # print(file)
+        # data = pandas.read_csv(file, encoding='utf-8').to_dict("records")
         data = json.load(file)
-        for doc in data: # canviar el district i el neighbourhood pels reconciled i afegir els IDs correspoents
+        # for doc in data: # canviar el district i el neighbourhood pels reconciled i afegir els IDs correspoents
+        if len(data) > 0:
+            collection.insert_many(data)
+        # print(json.dumps(data[2], indent=2))
+        # 2/0
 
-
-        # print(json.dumps(data[0], indent=2))
-        2/0
-        collection.insert_many(data)
     print("ara hi ha ", collection.count_documents({}), "documents")
