14be2165c98c6234c8c2578d905bfd87a5ff0bab
espasa99 <jordiespasarosell@gmail.com>
2023-04-11 18:17:24 +0200

    api_call

--- call_api.py
+++ call_api.py
@@ -1,4 +1,16 @@
-import urllib
-url = 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=416c6da9-f546-48f4-858d-836c25fa2ed3&limit=5&q=title:jones'
-fileobj = urllib.urlopen(url)
-print fileobj.read()
+import urllib.request
+import json
+
+url = 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=416c6da9-f546-48f4-858d-836c25fa2ed3&limit='
+limit_rows = 5
+
+with urllib.request.urlopen(url + str(limit_rows)) as url:
+    s = url.read()
+
+# Decode the bytes object into a string
+data_str = s.decode('utf-8')
+
+# Parse the string into a JSON object (dictionary)
+data_json = json.loads(data_str)
+
+print(data_json['result'])  
