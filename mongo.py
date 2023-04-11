a13a42e2f0d8bfdf71ff296c17ea9f5e7c0be1e8
G0002 <guillem.bosch.tobella@estudiantat.upc.edu>
2023-04-11 20:44:12 +0200

    mongo.py hehe

--- mongo.py
+++ mongo.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 # from os import listdir
 # import pandas
 
-directory = "idealista"
+directory = "temporal_landing/idealista"
 # directory = "opendatabcn-income"
 #idealista: mirar duplicats, afegir la data, fer que propertycode sigui el id.
 
@@ -18,9 +18,13 @@ print("ara hi ha ", collection.count_documents({}), "documents")
 for f in os.listdir(directory):
 
     with open(os.path.join(directory, f), 'r') as file:
-        # print(file)
+        print(f)
+        date = f[0:10]
         # data = pandas.read_csv(file, encoding='utf-8').to_dict("records")
         data = json.load(file)
+        for doc in data:
+            doc['date'] = date
+            collection.find({}
         # for doc in data: # canviar el district i el neighbourhood pels reconciled i afegir els IDs correspoents
         if len(data) > 0:
             collection.insert_many(data)
