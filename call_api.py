43927df0eb7322eb56345ec46f7e7c85f27c4dfb
espasa99 <jordiespasarosell@gmail.com>
2023-04-11 19:01:45 +0200

    .

--- call_api.py
+++ call_api.py
@@ -1,8 +1,9 @@
 import urllib.request
 import json
+import pandas as pd
 
 url = 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=416c6da9-f546-48f4-858d-836c25fa2ed3&limit='
-limit_rows = 5
+limit_rows = 2000
 
 with urllib.request.urlopen(url + str(limit_rows)) as url:
     s = url.read()
@@ -13,4 +14,14 @@ data_str = s.decode('utf-8')
 # Parse the string into a JSON object (dictionary)
 data_json = json.loads(data_str)
 
-print(data_json['result'])  
+# Transform the JSON records into a Pandas DataFrame
+records_data_frame = pd.DataFrame(data_json['result']['records'])
+print(records_data_frame)
+
+# Transform the JSON metadata into a Pandas DataFrame
+metadata_data_frame = pd.DataFrame(data_json['result']['fields'])
+print(metadata_data_frame)
+
+# save csv files
+records_data_frame.to_csv('./data_api/records_data_frame.csv', index=False)
+metadata_data_frame.to_csv('./data_api/metadata_data_frame.csv', index=False)
