8023a6551da98b897d84046ff2fefe9a2eb131e5
espasa99 <jordiespasarosell@gmail.com>
2023-04-12 00:26:29 +0200

    .

--- data_persistance_loaders/persistance_loader.py
+++ data_persistance_loaders/persistance_loader.py
@@ -2,7 +2,6 @@ from persistance_loader_funcs import connect_mongo, insert_data_to_mongo, insert
 
 temporal_landing_path = "./temporal_landing/"
 
-# Conectarse a MongoDB
 database_name = "persistent_landing"
 mongo_connection_string = "mongodb://localhost:27017/"
 data_base_mongo = connect_mongo(mongo_connection_string, database_name)
