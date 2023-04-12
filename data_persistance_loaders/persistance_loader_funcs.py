1f32c718f8289a87f22b5dadd67cdc2c333dd322
G0002 <guillem.bosch.tobella@estudiantat.upc.edu>
2023-04-12 21:00:15 +0200

    dupes

--- data_persistance_loaders/persistance_loader_funcs.py
+++ data_persistance_loaders/persistance_loader_funcs.py
@@ -85,7 +85,6 @@ def insert_idealista_data_mongo(temporal_landing_path: str, collection_name: str
     folder_path = f'{temporal_landing_path}{collection_name}/'
 
     collection = data_base_mongo[collection_name]
-    collection.delete_many({}) # (?) hace falta borrar la colección antes de insertar los datos?
 
     if insert_type[0] == 'all':
         files_list = [f for f in os.listdir(folder_path)]
@@ -96,15 +95,16 @@ def insert_idealista_data_mongo(temporal_landing_path: str, collection_name: str
     for file_name in files_list:
         
         file_path = os.path.join(folder_path, file_name)
-        year = file_name.split('_')[0]
+        date = '_'.join(file_name.split('_')[0:3])
 
         with open(file_path, 'r') as file:
 
             data = json.load(file)
 
             for doc in data:       # (?) fa falta afegir la data a cada document?
-                doc['date'] = year
+                doc['date'] = date
+                dup = collection.find_one({"propertyCode": doc["propertyCode"]})
+                if dup is None:
+                    collection.insert_one(doc)
 
-        if len(data) > 0:    # (?) hace falta comprobar que la lista no esté vacía
-            collection.insert_many(data)
             print(f"Los datos de {file_name} se han cargado correctamente en MongoDB.")
