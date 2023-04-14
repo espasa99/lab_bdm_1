7809b30ab2bf3b736b6a7dee2862f01bbf4f6955
G0002 <guillem.bosch.tobella@estudiantat.upc.edu>
2023-04-14 13:15:24 +0200

    hbase

--- data_persistance_loaders/persistance_loader.py
+++ data_persistance_loaders/persistance_loader.py
@@ -1,16 +1,17 @@
-from persistance_loader_funcs import connect_mongo, insert_csv_data_to_mongo, insert_json_data_mongo
+from persistance_loader_funcs import connect_hbase, insert_csv_data_to_hbase, insert_json_data_to_hbase
 
 UPDATE_FREQUENCY = 7 # days
-DATABASE_NAME = "persistent_landing"
+TABLE_NAME = "persistent_landing"
+VM_IP_ADDRESS = "192.168.1.62"
 temporal_landing_path = "./temporal_landing/"
 mongo_connection_string = "mongodb://localhost:27017/"
 
-data_base_mongo = connect_mongo(mongo_connection_string, DATABASE_NAME)
+hbase_table = connect_hbase(VM_IP_ADDRESS, TABLE_NAME)
 
 # Income data
 collection_name = "opendatabcn-income"
 update_type = 'complete'    # 'incremental' or 'complete' 
-insert_csv_data_to_mongo(temporal_landing_path, collection_name, data_base_mongo, update_type, UPDATE_FREQUENCY)
+insert_csv_data_to_hbase(temporal_landing_path, collection_name, hbase_table, update_type, UPDATE_FREQUENCY)
 
 # lookup_tables data
 collection_name = "lookup_tables"
--- data_persistance_loaders/persistance_loader_funcs.py
+++ data_persistance_loaders/persistance_loader_funcs.py
@@ -1,34 +1,39 @@
 import os
 import json
-import sqlite3
 import pandas as pd
-from pymongo import MongoClient
-from datetime import datetime, timedelta
+# from pymongo import MongoClient
+import happybase
+import sqlite3
+from datetime import datetime
 
-def connect_mongo(mongo_connection_string: str, database_name: str) -> object:
+
+def connect_hbase(connection_string: str, table_name: str) -> happybase.Connection:
     '''
-    Function to connect to MongoDB
+    Function to connect to HBase
 
     Parameters
     ----------
-    mongo_connection_string : str
-        Connection string to MongoDB
-    database_name : str
-        Name of the database to connect to
+    connection_string : str
+        Connection string to HBase, 'host:port'. If no port is specified default port is used.
+    table_name : str
+        Name of the table that will be returned.
 
     Returns
     -------
-    object
-        MongoDB database object
-        
+    happybase.Connection
+        HBase connection object
+
     '''
 
-    client = MongoClient(mongo_connection_string)
-    db = client[database_name]
+    # connection_string = "192.168.1.62"
 
-    return db
+    connection = happybase.Connection(connection_string, autoconnect=True)
 
-def insert_csv_data_to_mongo(temporal_landing_path: str, collection_name: str, data_base_mongo: object, update_type: str, update_frequency: int) -> None:
+
+    return connection
+
+
+def insert_csv_data_to_hbase(temporal_landing_path: str, source_name: str, hbase_table: happybase.Table, insert_type=['all']) -> None:
     '''
     Function to read and load the CSV files into MongoDB
 
@@ -36,85 +41,83 @@ def insert_csv_data_to_mongo(temporal_landing_path: str, collection_name: str, d
     ----------
     temporal_landing_path : str
         Path to the folder where the CSV files are stored
-    collection_name : str
-        Name of the collection where the data will be stored
-    data_base_mongo : object
-        MongoDB database object
-    update_type : string
-        Complete or incremental update. 
-        If the update its complete we will insert all the files in the folder, 
-        if not we will insert only the files modified since the last week in the 
-        temporal_landing by querying the register_upload database.
-        
+    source_name : str
+        Name of the source where data comes from
+    hbase_table : happybase.Table
+        HBase table object
+    insert_type : list, optional
+        List of files to insert. If 'all' is passed, all the files in the folder will be inserted, by default ['all']
+
     '''
-    
-    folder_path = f'{temporal_landing_path}{collection_name}/'
 
-    collection = data_base_mongo[collection_name]
+    folder_path = f'{temporal_landing_path}{source_name}/'
 
-    if update_type == 'complete':
+    if insert_type[0] == 'all':
         files_list = [f for f in os.listdir(folder_path)]
 
     else:
-        files_list = temporal_files_modified(collection_name, update_frequency)
+        files_list = insert_type
 
     for file_name in files_list:
 
         file_path = os.path.join(folder_path, file_name)
 
-        df_file = pd.read_csv(file_path)
-        data = df_file.to_dict("records")
+        file = open(file_path, 'r')
+        file_content = file.read().encode()
+        date = file_name.split('_')[0]
+        key = '$'.join([date, ]).encode()
 
-        if len(data) > 0:    
-            collection.insert_many(data)
+        hbase_table.put(key, {b'file:content': file_content})
 
-            date = file_name.split('_')[0]
-            
-            register_upload(date, file_name, 'json', collection_name)
-            print(f"Los datos de {file_name} se han cargado correctamente en MongoDB.")
+        # schema =
+        # table.put(key, {b'metadata:schema': schema})
 
-def insert_json_data_mongo(temporal_landing_path: str, collection_name: str, data_base_mongo: object, update_type: str, update_frequency: int) -> None:
+        register_upload(date, file_name, 'json', source_name)
+        print(f"Los datos de {file_name} se han cargado correctamente en HBase.")
+
+
+def insert_json_data_to_hbase(temporal_landing_path: str, source_name: str, hbase_table: happybase.Table, insert_type=['all']) -> None:
     '''
-    Function to read and load the idealista files into MongoDB
+    Function to read and load the idealista files into HBase
 
     Parameters
     ----------
     temporal_landing_path : str
         Path to the folder where the JSON files are stored
-    collection_name : str
-        Name of the collection where the data will be stored
-    data_base_mongo : object
-        MongoDB database object
-    update_type : string
-        Complete or incremental update. 
-        If the update its complete we will insert all the files in the folder, 
-        if not we will insert only the files modified since the last week in the 
-        temporal_landing by querying the register_upload database.
+    source_name : str
+        Name of the source where data comes from
+    hbase_table : happybase.Table
+        hbase table
+    insert_type : list, optional
+        List of files to insert. If 'all' is passed, all the files in the folder will be inserted, by default ['all']
+
     '''
-    
-    folder_path = f'{temporal_landing_path}{collection_name}/'
 
-    collection = data_base_mongo[collection_name]
+    folder_path = f'{temporal_landing_path}{source_name}/'
 
-    if update_type == 'complete':
+    if insert_type[0] == 'all':
         files_list = [f for f in os.listdir(folder_path)]
 
     else:
-        files_list = temporal_files_modified(collection_name, update_frequency)
+        files_list = insert_type
 
     for file_name in files_list:
-        
+
         file_path = os.path.join(folder_path, file_name)
         date = '_'.join(file_name.split('_')[0:3])
 
-        with open(file_path, 'r') as file:
+        file = open(file_path, 'r')
+        file_content = file.read().encode()
+        key = '$'.join([source_name, date]).encode()
 
-            data = json.load(file)      
-            if len(data) > 0:
-                collection.insert_many(data)
+        hbase_table.put(key, {b'file:content': file_content})
 
-            register_upload(date, file_name, 'json', collection_name)
-            print(f"Los datos de {file_name} se han cargado correctamente en MongoDB.")
+        # schema =
+        # table.put(key, {b'metadata:schema': schema})
+
+        register_upload(date, file_name, 'json', source_name)
+        print(f"Los datos de {file_name} se han cargado correctamente en MongoDB.")
+
 
 def register_upload(valid_date: str, file_name: str, file_format: str, collection_name: str) -> None:
     '''
@@ -136,34 +139,8 @@ def register_upload(valid_date: str, file_name: str, file_format: str, collectio
 
     now = datetime.now()
     upload_date = now.strftime("%Y/%m/%d %H:%M:%S")
-    query = f"INSERT INTO uploads_persitent_landing VALUES ('{upload_date}', '{valid_date}', '{file_name}', '{file_format}', '{collection_name}')"
+    query = f"INSERT INTO log VALUES ('{upload_date}', '{valid_date}', '{file_name}', '{file_format}', '{collection_name}')"
     c.execute(query)
 
     conn.commit()
     conn.close()
-
-def temporal_files_modified(collection_name: str, update_frequency: int) -> list:
-    '''
-    Function to get the files modified since the last week (or another frequency dependig on the config.) in the temporal_landing
-
-    Returns
-    -------
-    list
-        List of files modified since the last week in the temporal_landing
-    '''
-
-    conn = sqlite3.connect('./register_uploads/register_uploads.db')
-    c = conn.cursor()
-
-    today = datetime.now()
-    last_week = today - timedelta(days=update_frequency)
-    last_week = last_week.strftime("%Y/%m/%d %H:%M:%S")
-    
-    query = f"SELECT file_name FROM uploads_temporal_landing WHERE upload_date >= '{last_week}' AND collection_name = '{collection_name}'"
-    c.execute(query)
-
-    files_list = [file[0] for file in c.fetchall()]
-
-    conn.close()
-
-    return files_list
